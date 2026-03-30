3fde0c74be25ee51382533ac43acc7d11518e69f
Reshma66-web <reshma88450@gmail.com>
2026-03-30 13:00:02 +0530

    engineer2

--- PageObjectModel/login.page.js
+++ PageObjectModel/login.page.js
@@ -4,7 +4,7 @@ constructor(page){
     this.username = page.locator('input[name="user_name"]')
     this.password = page.locator('input[name="user_password"]')
     this.loginBtn = page.locator('#submitButton')
-    //eng1 - Engineer1
+    //eng2 - Engineer2
 }
 
 }
